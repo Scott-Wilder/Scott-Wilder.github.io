be8a3f19ad398cc666791ec47cf6596276e7dc21
Scott Wilder <53450610+Scott-Wilder@users.noreply.github.com>
2022-03-24 22:06:56 -0400

    fix JS

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ getText("https://api.adviceslip.com/advice");
 
 async function getText(file) {
   var myObject = await fetch(file);
-  var myText = await myObject.text();
-  document.getElementById("quote").innerHTML = myText;
+  var myText = await myObject.json();
+  document.getElementById("id").innerHTML = myText["slip"]["id"];
+  document.getElementById("quote").innerHTML = myText["slip"]["advice"];
 }
