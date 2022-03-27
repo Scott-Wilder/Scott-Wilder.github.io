c3495b43ede7e791a69124de34e3db777b5d9785
Scott Wilder <53450610+Scott-Wilder@users.noreply.github.com>
2022-03-27 14:08:41 -0400

    fixed js and updated readme
    added event listener that will trigger new quote generation

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-getText("https://api.adviceslip.com/advice");
-
-async function getText(file) {
-  var myObject = await fetch(file);
-  var myText = await myObject.json();
-  document.getElementById("id").innerHTML = myText["slip"]["id"];
-  document.getElementById("quote").innerHTML = myText["slip"]["advice"];
+const dice = document.getElementById("circle");
+const quote = document.getElementById("quote");
+const id = document.getElementById("id");
+//populates on load
+getData();
+// gets data from API and sets the content of #quote and #id p tag
+async function getData() {
+  quote.innerText = "Loading....";
+  try {
+    const res = await fetch(
+      "https://api.adviceslip.com/advice?t=" + Math.random()
+    );
+    var myText = await res.json();
+    quote.innerHTML = myText["slip"]["advice"];
+    id.innerHTML = myText["slip"]["id"];
+  } catch (error) {
+    console.log(error);
+  }
 }
+// add event listener for #circle div
+dice.addEventListener("click", getData);
